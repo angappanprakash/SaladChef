27a13ad20afcde02c8575cb80661d721a1ce1aec
angappanprakash <37359602+angappanprakash@users.noreply.github.com>
2018-03-16 15:42:34 +0530

    First commit - Initial Project setup
    Created basic project setup and folder structure.

---
+++ Assets/Scripts/Framework/SCResourceLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SCResourceLoader : MonoBehaviour
+{
+	public static void LoadScene(string sceneName, bool waitForInput = false)
+	{
+		if (string.IsNullOrEmpty(sceneName))
+			return;
+		SceneManager.LoadScene("MainMenu");
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+#region Variables
+	public const int MAX_LOCAL_PLAYER_COUNT = 2;
+
+	private static GameManager m_Instance;
+#endregion
+
+#region Properties
+	public static GameManager Instance
+	{
+		get { return m_Instance; }
+	}
+#endregion
+
+#region Monobehaviour functions
+	private void Awake()
+	{
+		m_Instance = this;
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	private void Update()
+	{
+	}
+
+	private void OnDestroy()
+	{
+		m_Instance = null;
+	}
+#endregion
+}
